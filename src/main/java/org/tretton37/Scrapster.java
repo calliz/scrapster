5ffbb8e4c218e122d77e950578403678a9a6192b
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2024-01-18 12:55:11 +0100

    Use HttpClient to fetch pages

--- src/main/java/org/tretton37/Scrapster.java
+++ src/main/java/org/tretton37/Scrapster.java
@@ -1,25 +1,24 @@
 package org.tretton37;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Scrapster {
     public static final String URL = "https://books.toscrape.com/";
     // Keep track of visited links
-    private final Set<String> visited;
+    private static Set<String> visited = new HashSet<>();
 
     // Avoid stackoverflow and loops
-    private static final int MAX_DEPTH = 2;
-
-    public Scrapster() {
-        visited = new HashSet<>();
-    }
+    private static final int MAX_DEPTH = 1;
 
     private void scrapePage(String url, int maxDepth) {
         if (maxDepth < MAX_DEPTH && !visited.contains(url)) {
@@ -27,20 +26,36 @@ public class Scrapster {
                 // Add if not already visited
                 visited.add(url);
 
-                // Fetch page
-                Document doc = Jsoup.connect(url).get();
+                // Get html page content using HttpClient to make the scraping blocking
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request;
+                try {
+                    request = HttpRequest.newBuilder()
+                                         .uri(URI.create(url))
+                                         .build();
+                } catch (IllegalArgumentException e) {
+                    return;
+                }
 
-                // Get page content
-                String content = doc.outerHtml();
+                HttpResponse<String> response;
+                try {
+                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                } catch (IOException |
+                         InterruptedException e) {
+                    return;
+                }
 
-                // Save content
-                savePageContent(url, content);
+                String pageContent = response.body();
+
+                // Save content and continue
+                savePageContent(url, pageContent);
 
                 // Get all page links
-                Elements links = doc.select("a[href]");
+                Elements pageLinks = Jsoup.parse(pageContent, url)
+                                          .select("a[href]");
 
                 // For each link, increase depth and fetch and save content recursively
-                for (Element link : links) {
+                for (Element link : pageLinks) {
                     scrapePage(link.attr("abs:href"), maxDepth + 1);
                 }
             } catch (IOException e) {
