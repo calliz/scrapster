fa4af220de8be976dd28a318af618e01ead5a067
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2024-01-18 12:56:59 +0100

    Use WebFlux webclient instead, which is non-blocking
    blockLast() is used to make sure that all scraping threads are finished
    before the application shuts down.  Could be improved by using Spring
    boot for a real web application but for a console application this is
    probbably fine.

--- src/main/java/org/tretton37/Scrapster.java
+++ src/main/java/org/tretton37/Scrapster.java
@@ -4,15 +4,18 @@ import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,7 +25,11 @@ public class Scrapster {
     private static Set<String> visited = new HashSet<>();
 
     // Avoid stackoverflow and loops
-    private static final int MAX_DEPTH = 1;
+    private static final int MAX_DEPTH = 2;
+
+    private static final WebClient WEB_CLIENT = WebClient.builder()
+                                                         .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE)
+                                                         .build();
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Scrapster.class);
     private static int skippedMaxDepth = 0;
@@ -43,53 +50,58 @@ public class Scrapster {
         // Add if not already visited
         visited.add(url);
 
-        // Get html page content using HttpClient to make the scraping blocking
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request;
-        try {
-            request = HttpRequest.newBuilder()
-                                 .uri(URI.create(url))
-                                 .build();
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("Failed to create request for url: {} ", url, e);
-            return Flux.empty();
-        }
+        // Get html page content using webclient to make the scraping non-blocking
+        Mono<String> pageContent = WEB_CLIENT.get()
+                                             .uri(url)
+                                             .retrieve()
+                                             .onStatus(HttpStatus::isError, clientResponse -> Mono.error(new RuntimeException("Error fetching url: " + url)))
+                                             .bodyToMono(String.class)
+                                             .doOnSubscribe(subscription -> LOGGER.info("Starting fetch for url: {}", url))
+                                             .doOnSuccess(content -> {
+                                                 LOGGER.debug("Completed fetch for url: {}", url);
+                                                 // LOGGER.debug("Content for url {} : \n{}", url, content);
+                                             })
+                                             .flatMap(content -> {
+                                                 LOGGER.info("Fetched content from url: {}", url);
+                                                 // Save content and continue
+                                                 return savePageContent(content, url).thenReturn(content);
+                                             })
+                                             /*.onErrorResume(e -> {
+                                                 LOGGER.error("Error fetching content from url: {}", url, e);
+                                                 return Mono.empty();
+                                             })*/;
 
-        HttpResponse<String> response;
-        try {
-            LOGGER.debug("Send http request to: {}", url);
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException |
-                 InterruptedException e) {
-            LOGGER.error("Error getting response from url: {}", url, e);
-            return Flux.empty();
-        }
 
-        String pageContent = response.body();
-        // LOGGER.debug("Fetched content: {}", pageContent);
+        return pageContent.flatMapMany(content -> {
+            // Get all page links
+            Elements pageLinks = Jsoup.parse(content)
+                                      .select("a[href]");
 
-        // Save content and continue
-        savePageContent(pageContent, url);
-
-        // Get all page links
-        Elements pageLinks = Jsoup.parse(pageContent, url)
-                                  .select("a[href]");
-
-        // For each link, increase depth and fetch and save content recursively
-        Flux<Object> resultFlux = Flux.fromIterable(pageLinks)
-                                      // get raw link
-                                      .map(link -> link.attr("href"))
-                                      // Add base url if relative link
-                                      .map(link -> link.startsWith("http://") || link.startsWith("https://") ? link : URL + link)
-                                      .doOnNext(link -> LOGGER.debug("Scraping link: {}", link))
-                                      .flatMap(link -> scrapePage(link, maxDepth + 1));
-
-        return resultFlux;
+            // For each link, increase depth and fetch and save content recursively
+            return Flux.fromIterable(pageLinks)
+                       // get raw link
+                       .map(link -> link.attr("href"))
+                       // Add base url if relative link
+                       .map(link -> {
+                           try {
+                               URI uri = new URI(link);
+                               if (uri.isAbsolute()) {
+                                   return link;
+                               } else {
+                                   return URL + link;
+                               }
+                           } catch (URISyntaxException e) {
+                               LOGGER.debug("Failed to parse link: " + link);
+                               return URL + link;
+                           }
+                       })
+                       .doOnNext(link -> LOGGER.debug("Scraping link: {}", link))
+                       .flatMap(link -> scrapePage(link, maxDepth + 1));
+        });
     }
 
-    private static void savePageContent(String content, String url) {
+    private static Mono<Void> savePageContent(String content, String url) {
         LOGGER.debug("Saving page content for url: {}", url);
-
         String filename = Math.abs(url.hashCode()) + ".txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             writer.write(content);
@@ -97,6 +109,7 @@ public class Scrapster {
         } catch (IOException e) {
             LOGGER.error("Error: failed to save page content for url {} to disk.", url, e);
         }
+        return Mono.empty();
     }
 
     public static void main(String[] args) {
@@ -106,6 +119,6 @@ public class Scrapster {
                                "Skipped due to max depth: {} urls\n" +
                                "Skipped due to already visited: {} urls\n" +
                                "Urls visited {}", skippedMaxDepth, skippedVisited, visited.size()))
-                       .subscribe();
+                       .blockLast();
     }
 }
