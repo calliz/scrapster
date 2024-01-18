5925a76af2c54d40f9a38658105f26ce98d4afb9
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2024-01-18 12:54:08 +0100

    Add recursive scrape
    - Max depth to avoid stackoverflow or loops

--- src/main/java/org/example/Scrapster.java
+++ src/main/java/org/example/Scrapster.java
@@ -2,32 +2,60 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Scrapster {
+    public static final String URL = "https://books.toscrape.com/";
+    // Keep track of visited links
+    private final Set<String> visited;
 
-    private void scrapePage(String url) {
-        try {
-            // Fetch page
-            Document doc = Jsoup.connect(url).get();
-            // Get page content
-            String content = doc.outerHtml();
+    // Avoid stackoverflow and loops
+    private static final int MAX_DEPTH = 2;
 
-            // Save content
-            savePageContent(url, content);
+    public Scrapster() {
+        visited = new HashSet<>();
+    }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void scrapePage(String url, int maxDepth) {
+        if (maxDepth < MAX_DEPTH && !visited.contains(url)) {
+            try {
+                // Add if not already visited
+                visited.add(url);
+
+                // Fetch page
+                Document doc = Jsoup.connect(url).get();
+
+                // Get page content
+                String content = doc.outerHtml();
+
+                // Save content
+                savePageContent(url, content);
+
+                // Get all page links
+                Elements links = doc.select("a[href]");
+
+                // For each link, increase depth and fetch and save content recursively
+                for (Element link : links) {
+                    scrapePage(link.attr("abs:href"), maxDepth + 1);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private static void savePageContent(String url, String content) throws IOException {
+    private void savePageContent(String url, String content) throws IOException {
         System.out.println(url);
         // System.out.println(content);
+        System.out.println(visited.size() + " urls visited");
     }
 
     public static void main(String[] args) {
-        new Scrapster().scrapePage("http://example.com");
+        new Scrapster().scrapePage(URL, 0);
     }
 }
