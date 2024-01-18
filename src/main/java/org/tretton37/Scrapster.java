52e4ab7a8a5bd72f9f5075be91b2ed4d3bbf6fd1
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2024-01-18 12:56:06 +0100

    Add WebFlux streams to simplify scraping and parsing
    - HttpClient is blocking
    - WebClient is non-blocking but doesn't work
    - Fix error with relative urls missing base url and http/https

--- src/main/java/org/tretton37/Scrapster.java
+++ src/main/java/org/tretton37/Scrapster.java
@@ -1,8 +1,8 @@
 package org.tretton37;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import reactor.core.publisher.Flux;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -20,46 +20,55 @@ public class Scrapster {
     private static Set<String> visited = new HashSet<>();
 
     // Avoid stackoverflow and loops
-    private static final int MAX_DEPTH = 1;
+    private static final int MAX_DEPTH = 2;
 
-    private void scrapePage(String url, int maxDepth) {
-        if (maxDepth < MAX_DEPTH && !visited.contains(url)) {
-            // Add if not already visited
-            visited.add(url);
-
-            // Get html page content using HttpClient to make the scraping blocking
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request;
-            try {
-                request = HttpRequest.newBuilder()
-                                     .uri(URI.create(url))
-                                     .build();
-            } catch (IllegalArgumentException e) {
-                return;
-            }
-
-            HttpResponse<String> response;
-            try {
-                response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException |
-                     InterruptedException e) {
-                return;
-            }
-
-            String pageContent = response.body();
-
-            // Save content and continue
-            savePageContent(url, pageContent);
-
-            // Get all page links
-            Elements pageLinks = Jsoup.parse(pageContent, url)
-                                      .select("a[href]");
-
-            // For each link, increase depth and fetch and save content recursively
-            for (Element link : pageLinks) {
-                scrapePage(link.attr("abs:href"), maxDepth + 1);
-            }
+    private Flux<Object> scrapePage(String url, int maxDepth) {
+        if (maxDepth >= MAX_DEPTH) {
+            return Flux.empty();
+        } else if (visited.contains(url)) {
+            return Flux.empty();
         }
+
+        // Add if not already visited
+        visited.add(url);
+
+        // Get html page content using HttpClient to make the scraping blocking
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request;
+        try {
+            request = HttpRequest.newBuilder()
+                                 .uri(URI.create(url))
+                                 .build();
+        } catch (IllegalArgumentException e) {
+            return Flux.empty();
+        }
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException |
+                 InterruptedException e) {
+            return Flux.empty();
+        }
+
+        String pageContent = response.body();
+
+        // Save content and continue
+        savePageContent(pageContent, url);
+
+        // Get all page links
+        Elements pageLinks = Jsoup.parse(pageContent, url)
+                                  .select("a[href]");
+
+        // For each link, increase depth and fetch and save content recursively
+        Flux<Object> resultFlux = Flux.fromIterable(pageLinks)
+                                      // get raw link
+                                      .map(link -> link.attr("href"))
+                                      // Add base url if relative link
+                                      .map(link -> link.startsWith("http://") || link.startsWith("https://") ? link : URL + link)
+                                      .flatMap(link -> scrapePage(link, maxDepth + 1));
+
+        return resultFlux;
     }
 
     private static void savePageContent(String content, String url) {
@@ -75,6 +84,7 @@ public class Scrapster {
     }
 
     public static void main(String[] args) {
-        new Scrapster().scrapePage(URL, 0);
+        new Scrapster().scrapePage(URL, 0)
+                       .subscribe();
     }
 }
